c0535f50596a7fcbbfdba6bb808b532e4c7289da
Tahir Wasim <tahir.wasim@groupbyinc.com>
2017-05-11 14:02:47 -0400

    write tests

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+const logLevels = [
+  'trace',
+  'debug',
+  'info',
+  'warn',
+  'error',
+  'fatal'
+];
+
 const Stream = function (sdc, serviceName, threshold) {
   const self      = this;
-  const logLevels = [
-    'trace',
-    'debug',
-    'info',
-    'warn',
-    'error',
-    'fatal'
-  ];
+
+  if (!sdc || typeof sdc != 'object' || !sdc.increment || typeof sdc.increment != 'function'){
+    throw new Error('sdc must be an object with the function \'increment()\'');
+  }
+
+  if (!serviceName || typeof serviceName != 'string'){
+    throw new Error ('serviceName must be of type string');
+  }
+
+  if (logLevels.indexOf(threshold) === -1){
+    throw new Error(`threshold must be one of: ${logLevels}`);
+  }
 
   self.thresholdLevel = (logLevels.indexOf(threshold) + 1) * 10;
 
@@ -15,16 +28,17 @@ const Stream = function (sdc, serviceName, threshold) {
 
     const logLevelNumeric = data.level;
     if (logLevelNumeric >= self.thresholdLevel) {
-      const logIndex = (logLevelNumeric/10)-1;
+      const logIndex = (logLevelNumeric / 10) - 1;
       sdc.increment(`${serviceName}.log.${logLevels[logIndex]}`);
     }
 
   };
 
-  self.end = () => {};
+  self.end = () => {
+  };
 
   return self;
 };
 
-
 module.exports = Stream;
+
--- test/index.spec.js
+++
@@ -1,9 +0,0 @@
-const Promise    = require('bluebird');
-const chai       = require('chai');
-const expect     = chai.expect;
-const spies      = require('chai-spies');
-const HTTPStatus = require('http-status');
-
-describe('Sysdig logger stream', () => {
-
-});
